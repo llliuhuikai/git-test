f5577beea89911a06fd1cb73cdfccc1a8be46ed2
liuhuikai <510328841@qq.com>
2020-08-20 22:34:38 +0800

    111

---
+++ git/src/com/bjpowernode/git/MyGit.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.git;
+
+public class MyGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
